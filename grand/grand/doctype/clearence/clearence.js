7e1aa5062657c90a31d9f4931a71c8d477ab4915
Connect <connect@vmi2135316.contaboserver.net>
2026-01-04 20:44:40 +0200

    commit

---
+++ grand/grand/doctype/clearence/clearence.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Connect 4 Systems and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Clearence", {
+// 	refresh(frm) {
+
+// 	},
+// });
