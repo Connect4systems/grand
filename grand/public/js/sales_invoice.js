b323dcf9e5e31546db7bf34604800268c8dd80b2
Connect4 <connect4systems@gmail.com>
2026-01-04 19:48:29 +0200

    10

--- grand/public/js/sales_invoice.js
+++ grand/public/js/sales_invoice.js
@@ -1,50 +1,46 @@
 frappe.ui.form.on("Sales Invoice", {
     refresh: function(frm){
-        if (frm.doc.docstatus == 1 && frm.doc.contractor_order && (frm.doc.deductions && frm.doc.deductions.length || frm.doc.deduction_table && frm.doc.deduction_table.length)){
-            frm.add_custom_button(__('Deduction Entry'), () => {
+        if (frm.doc.docstatus == 1 && frm.doc.contractor_order && frm.doc.deductions){
+            frm.add_custom_button(__("Deduction Entry"), () => {
                 frm.events.create_deduction_entry(frm);
             }, __('Create'))
         }
     },
     create_deduction_entry: function(frm){
-        // Default mapping - you can change method to your mapping function
         frappe.model.open_mapped_doc({
             method: "contractor.contractor_app.doctype.clearence.clearence.create_a_payment",
             frm: frm
         })
     },
-    selling_deductions_template: function(frm){
+    selling_deductions_template() {
         if(frm.doc.selling_deductions_template) {
-            frappe.call({
+            return frm.call({
                 method: "contractor.www.api.get_deductions",
                 args: {
-                    "master_doctype": frappe.meta.get_docfield(frm.doc.doctype, "selling_deductions_template").options,
+                    "master_doctype": frappe.meta.get_docfield(frm.doc.doctype, "selling_deductions_template",
+                        frm.doc.name).options,
                     "master_name": frm.doc.selling_deductions_template
                 },
                 callback: function(r) {
                     if(!r.exc) {
-                        const rows = r.message || [];
-                        if (frm.doc.deductions && frm.doc.deductions.length) {
-                            for (let ded of rows) {
+                        if(frm.doc.deductions) {
+                            for (let ded of r.message) {
                                 frm.add_child("deductions", ded);
                             }
-                        } else if (rows.length) {
-                            frm.set_value("deductions", rows);
+
+                            refresh_field("deductions");
+                        } else {
+                            frm.set_value("deductions", r.message);
                         }
-                        frm.refresh_field("deductions");
                     }
                 }
             });
         }
     }
-});
-
+})
 frappe.ui.form.on("Deduction", {
     percent: function(frm, cdt, cdn){
         let row = locals[cdt][cdn];
-        const percent = parseFloat(row.percent) || 0;
-        const base = parseFloat(frm.doc.rounded_total) || 0;
-        const value = (percent * base / 100);
-        frappe.model.set_value(cdt, cdn, "value", flt(value, 2));
+        frappe.model.set_value(cdt, cdn, "value", row.percent * frm.doc.rounded_total / 100);
     }
-});
+})
